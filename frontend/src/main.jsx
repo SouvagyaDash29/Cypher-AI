73f6605549e3e96957cd6fbf582ec6410675ec02
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-08-27 13:52:48 +0530

    create routing for all layout pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,55 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Chatpage from "./pages/Chatpage";
+import LayoutPage from "./layouts/LayoutPage";
+import DashboardLayout from "./layouts/DashboardLayout";
+import SignIn from "./pages/signIn";
+import SignUp from "./pages/signUp";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    element: <LayoutPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/sign-in/*",
+        element: <SignIn />,
+      },
+      {
+        path: "/sign-up/*",
+        element: <SignUp />,
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <Dashboard />
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <Chatpage />
+          }
+        ]
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
