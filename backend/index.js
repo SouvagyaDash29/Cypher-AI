738011e294e49bb5851470f767a5a5325dc14a29
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-12-20 21:27:10 +0530

    update svix code

--- backend/index.js
+++ backend/index.js
@@ -54,37 +54,72 @@ app.get("/api/upload", (req, res) => {
 //   res.send("Success!");
 // })
 
-app.post('/api/webhooks',
-  bodyParser.raw({ type: "application/json" }),
-  function(req, res) {
-    try {
-      const payloadString = req.body.toString();
-      const svixHeaders = req.headers;
+app.post(
+  '/api/webhooks',
+  // This is a generic method to parse the contents of the payload.
+  // Depending on the framework, packages, and configuration, this may be
+  // different or not required.
+  bodyParser.raw({ type: 'application/json' }),
 
-      const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET_KEY)
-      const evt = wh.verify(payloadString, svixHeaders);
+  async (req, res) => {
+    const SIGNING_SECRET = process.env.CLERK_WEBHOOK_SECRET_KEY
 
-      const { id, ...attributes } = evt.data;
+    if (!SIGNING_SECRET) {
+      throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env')
+    }
 
-      const eventType = evt.type;
+    // Create new Svix instance with secret
+    const wh = new Webhook(SIGNING_SECRET)
 
-      if(eventType === 'user.created'){
-        console.log(`User ${id} is ${eventType}`)
-        console.log(attributes)
-      }
+    // Get headers and body
+    const headers = req.headers
+    const payload = req.body
 
-      res.status(200).json({
-        success: true,
-        message: 'Webhook received successfully',
-      })
+    // Get Svix headers for verification
+    const svix_id = headers['svix-id']
+    const svix_timestamp = headers['svix-timestamp']
+    const svix_signature = headers['svix-signature']
 
-    } catch (error) {
-      res.status(400).json({
+    // If there are no headers, error out
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+      return void res.status(400).json({
         success: false,
-        message: error.message,
+        message: 'Error: Missing svix headers',
       })
     }
-  })
+
+    let evt
+
+    // Attempt to verify the incoming webhook
+    // If successful, the payload will be available from 'evt'
+    // If verification fails, error out and return error code
+    try {
+      evt = wh.verify(payload, {
+        'svix-id': svix_id ,
+        'svix-timestamp': svix_timestamp ,
+        'svix-signature': svix_signature ,
+      })
+    } catch (err) {
+      console.log('Error: Could not verify webhook:', err.message)
+      return void res.status(400).json({
+        success: false,
+        message: err.message,
+      })
+    }
+
+    // Do something with payload
+    // For this guide, log payload to console
+    const { id } = evt.data
+    const eventType = evt.type
+    console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+    console.log('Webhook payload:', evt.data)
+
+    return void res.status(200).json({
+      success: true,
+      message: 'Webhook received',
+    })
+  },
+)
 
 
 app.post("/api/chats", ClerkExpressRequireAuth(), async (req, res) => {
@@ -205,7 +240,6 @@ app.listen(port, () => {
   connect();
   console.log(`listening on ${port}`);
 });
-
 
 
 
